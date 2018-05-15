346b08fd3ea5b69d91a42731135f5d7f71c34fe7
lachlanh <lhespe@opentable.com>
2018-05-15 14:03:47 -0700

    timelimit implemented

--- gul.go
+++ gul.go
@@ -9,13 +9,19 @@ import (
 )
 
 var num = flag.Int("n", 1, "number of requests")
+var timeLimit = flag.Duration("t", 0, "duration of how long to test")
 
 func main() {
 	//flag.PrintDefaults()
 	flag.Parse()
 
 	fmt.Println("num :", *num)
+	fmt.Println("timeLimit : ", *timeLimit)
 	fmt.Println("args : ", flag.Args())
+
+	if timeLimit != nil {
+		*num = 50000 //similar to ab
+	}
 
 	if len(flag.Args()) == 0 {
 		fmt.Println("specify a url")
@@ -25,7 +31,7 @@ func main() {
 	urlRaw := flag.Args()[0]
 	fmt.Println("urlRaw : ", urlRaw)
 	client := &http.Client{}
-	result := runUrl(urlRaw, *num, client)
+	result := runUrl(urlRaw, *num, *timeLimit, client)
 	reportResult(result)
 
 }
@@ -40,43 +46,55 @@ type response struct {
 }
 
 type runResult struct {
-	successCount int
-	errorCount   int
-	responses    []response
+	totalRequests int
+	successCount  int
+	errorCount    int
+	startTime     time.Time
+	endTime       time.Time
+	responses     []response
 }
 
-func runUrl(urlRaw string, it int, client HttpClient) runResult {
+func runUrl(urlRaw string, it int, timeLimit time.Duration, client HttpClient) runResult {
 	// TODO LH all the dynamic stuff and then collection of the results
 	var result runResult
 	result.responses = make([]response, 0)
-
+	result.startTime = time.Now()
 	for i := 0; i < it; i++ {
+		result.totalRequests++
 		start := time.Now()
 		resp, err := client.Get(urlRaw)
 		elapsed := time.Since(start)
 		if err != nil {
 			result.errorCount++
-			fmt.Println("err : ", err)
-			continue // TODO LH not sure how I feel about this
+		} else {
+			result.successCount++
+			response := response{
+				status:   resp.StatusCode,
+				duration: elapsed,
+			}
+			//resp.Close()
+			result.responses = append(result.responses, response)
 		}
-		result.successCount++
-		response := response{
-			status:   resp.StatusCode,
-			duration: elapsed,
+		if time.Since(result.startTime) >= timeLimit {
+			fmt.Println("time limit hit, break")
+			break
 		}
-		result.responses = append(result.responses, response)
 
 	}
-
+	result.endTime = time.Now()
 	return result
 }
 
 func reportResult(result runResult) {
+	fmt.Println("Total Requests : ", result.totalRequests)
 	fmt.Println("Success Count : ", result.successCount)
 	fmt.Println("Error Count : ", result.errorCount)
-	for _, response := range result.responses {
-		fmt.Println("resp status : ", response.status)
-		fmt.Println("elapsed : ", response.duration)
-	}
-
+	elapsed := result.endTime.Sub(result.startTime).Seconds()
+	fmt.Println("Elapsed : ", elapsed)
+	rps := float64(result.totalRequests) / float64(elapsed)
+	fmt.Println("RPS : ", rps)
+	// for _, response := range result.responses {
+	// 	fmt.Println("resp status : ", response.status)
+	// 	fmt.Println("elapsed : ", response.duration)
+	// }
 }
--- gul_test.go
+++ gul_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +18,20 @@ func (c *HttpClientMock) Get(url string) (*http.Response, error) {
 
 func TestRunUrl(t *testing.T) {
 	clientMock := HttpClientMock{}
-	result := runUrl("http://127.0.0.1", 10, &clientMock)
+	result := runUrl("http://127.0.0.1", 10, 10, &clientMock)
 	assert.NotNil(t, result)
 	assert.Equal(t, 10, result.successCount)
+}
+
+func TestCheckTiming(t *testing.T) {
+	timeLimit, _ := time.ParseDuration("1s")
+	start := time.Now()
+	time.Sleep(timeLimit)
+	elapsed := time.Since(start)
+	fmt.Println("elapsed : ", elapsed)
+	if time.Since(start) >= timeLimit {
+		fmt.Println("here we are")
+	}
+	assert.Fail(t, "fail")
+
 }
